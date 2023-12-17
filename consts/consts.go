5ed574fb22ef35020530d755fcc59cecb59c048d
ethParth <parthverma.eth@gmail.com>
2023-12-17 14:17:28 +0530

    Init Commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Xper"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "XperVM"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "XV"
 )
 
 var ID ids.ID
